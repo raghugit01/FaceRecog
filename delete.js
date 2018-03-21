4349981c314a15a6045cef88a7375d213989fac5
shwetank_kumar <shwetank.kumar@capgemini.com>
2018-03-21 11:27:18 +0530

    CRUD operation complete, now adding extra features

--- delete.js
+++ delete.js
@@ -1,9 +1,19 @@
-import * as dynamoDbLib from "./libs/dynamodb-lib";
-import {success, failure} from "./libs/response-lib";
+import * as dynamoDbLib from './libs/dynamodb-lib';
+import { success, failure } from './libs/response-lib';
 
-export const main = async (event, context, callback) {
+export const main = async (event, context, callback) => {
   const params = {
-    TableName: "events",
+    TableName: 'events',
+    Key: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      eventId: event.pathParameters.id,
+    },
+  };
 
+  try {
+    const result = await dynamoDbLib.call('delete', params);
+    callback(null, success({ status: true }));
+  } catch (e) {
+    callback(null, failure({ status: false }));
   }
-}
+};
